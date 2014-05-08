5c0090eb1a2200830364a5b2905937d6e9010a5a
Mangirdas <mangirdas@impresspages.org>
2014-05-08 15:36:22 +0300

    fix special characters issues on textarea field

--- ip_cms/modules/developer/form/Field/Textarea.php
+++ ip_cms/modules/developer/form/Field/Textarea.php
@@ -8,18 +8,18 @@ namespace Modules\developer\form\Field;
 
 
 class Textarea extends Field{
-    
+
     public function render($doctype) {
         $attributesStr = '';
-        
-        return '<textarea '.$this->getAttributesStr($doctype).' class="ipmControlTextarea '.implode(' ',$this->getClasses()).'" name="'.htmlspecialchars($this->getName()).'" '.$this->getValidationAttributesStr($doctype).' >'.htmlentities($this->getDefaultValue()).'</textarea>';
+
+        return '<textarea '.$this->getAttributesStr($doctype).' class="ipmControlTextarea '.implode(' ',$this->getClasses()).'" name="'.htmlspecialchars($this->getName()).'" '.$this->getValidationAttributesStr($doctype).' >'.htmlspecialchars($this->getDefaultValue(), ENT_QUOTES, 'UTF-8').'</textarea>';
     }
-    
+
     /**
     * CSS class that should be applied to surrounding element of this field. By default empty. Extending classes should specify their value.
     */
     public function getTypeClass() {
         return 'textarea';
     }
-    
+
 }
